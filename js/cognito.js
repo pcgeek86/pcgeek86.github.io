3379cc9f52aaa308b01bac3e656555b4584017cd
Trevor Sullivan <tsulli@amazon.com>
2018-04-09 17:58:59 -0700

    Updates

--- js/cognito.js
+++ js/cognito.js
@@ -3,4 +3,3 @@ AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'us-east-1:25123139-7725-4bd0-9763-c7520cc239a4',
 });
-console.log(`Loaded Amazon Cognito`);
