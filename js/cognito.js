39f3f7c4a0d14506e647402498e800d4ad16ec5a
Trevor Sullivan <tsulli@amazon.com>
2018-04-09 17:38:54 -0700

    Added Amazon Cognito

---
+++ js/cognito.js
@@ -0,0 +1,6 @@
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:25123139-7725-4bd0-9763-c7520cc239a4',
+});
+console.log(`Loaded Amazon Cognito`);
